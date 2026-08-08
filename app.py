1d5fb0386b7c44a362780dcb3410ae7eee8d4933
PujithaDumpa <pujithadumpa16@gmail.com>
2026-08-08 16:15:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,5 @@
 import streamlit as st
-import torch
-from huggingface_hub import hf_hub_download
 
-st.title("GAT Fraud Detection")
+st.title("Test App")
 
-st.write("App started")
-
-DEVICE = torch.device("cpu")
-
-st.write("Trying to load graph...")
-
-data_path = hf_hub_download(
-    repo_id="PujithaDumpa/elliptic-gat-data",
-    filename="elliptic_data.pt",
-    repo_type="model"
-)
-
-st.write("Graph file downloaded")
-
-data = torch.load(
-    data_path,
-    map_location=DEVICE,
-    weights_only=False
-)
-
-st.success("Graph loaded successfully!")
-
-st.write("Number of transactions:", data.num_nodes)
-st.write("Number of features:", data.num_features)
+st.success("Streamlit is working!")
