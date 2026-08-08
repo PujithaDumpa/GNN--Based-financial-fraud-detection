db12033ca865ebd739fccf3b5f6f3ac3057bfcae
PujithaDumpa <pujithadumpa16@gmail.com>
2026-08-08 16:19:28 +0530

    Update title and add data downloading feature

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 import streamlit as st
 import torch
+from huggingface_hub import hf_hub_download
 
-st.title("GAT Test")
+st.title("Dataset Test")
 
-st.success("Streamlit is working!")
+st.write("Streamlit: ✅")
+st.write("PyTorch:", torch.__version__)
 
-st.write("PyTorch version:", torch.__version__)
+st.write("Downloading graph...")
+
+data_path = hf_hub_download(
+    repo_id="PujithaDumpa/elliptic-gat-data",
+    filename="elliptic_data.pt",
+    repo_type="model"
+)
+
+st.success("Graph file downloaded successfully!")
+
+st.write("File path:", data_path)
